45b837f5fbcd25e8e55493d14327f9350a64f172
zuviabrand <zuviabrand@gmail.com>
2025-12-14 18:00:26 -0600

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAi6RcOg_8pLaj337D5MJw4LpU2QfMieu4",
+  authDomain: "zuvia-tracker.firebaseapp.com",
+  projectId: "zuvia-tracker",
+  storageBucket: "zuvia-tracker.firebasestorage.app",
+  messagingSenderId: "428384622870",
+  appId: "1:428384622870:web:7573e73262194b182ad837"
+};
